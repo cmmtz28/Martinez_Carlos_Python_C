cf670a44d96840bf6557b67d0dc6d7826ba639b2
cmmtz28 <133311024+cmmtz28@users.noreply.github.com>
2023-05-12 00:44:47 -0600

    Create Ejercicio_002.py

---
+++ Diagnóstico/Ejercicio_002.py
@@ -0,0 +1,6 @@
+"""Desarrollar las primeras líneas en Python creando un formulario en el que te preguntará lo siguiente:
+1.	¿Cuál es tu nombre(s)?
+2.	¿Cuál es tu primer apellido?
+3.	¿Cuál es tu segundo apellido?
+4.	¿En qué año naciste?
+5.	¿En qué mes y día naciste? (en el siguiente formato “mm-dd”)"""
