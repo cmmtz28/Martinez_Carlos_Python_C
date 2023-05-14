bb8f933e642efdee5f2b002eac6e23c5c73f6398
cmmtz28 <133311024+cmmtz28@users.noreply.github.com>
2023-05-13 23:15:10 -0600

    Update Ejercicio_002.py

--- Diagnóstico/Ejercicio_002.py
+++ Diagnóstico/Ejercicio_002.py
@@ -6,3 +6,8 @@
 5.	¿En qué mes y día naciste? (en el siguiente formato “mm-dd”)"""
 
 año=2023
+nom=input("¿Cuál es tu nombre(s)?")
+pa=input("¿Cuál es tu primer apellido?")
+sa=input("¿Cuál es tu segundo apellido?")
+an=input("¿En qué año naciste?")
+md=input("¿En qué mes y día naciste? (en el siguiente formato “mm-dd”)")
